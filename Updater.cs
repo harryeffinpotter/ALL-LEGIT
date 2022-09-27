3b2bcbb302e3010cb98934d2d77581d4c9922e1d
HarryEffinPotter <thisisyourknife@gmail.com>
2022-09-27 03:26:06 -0400

    1.1.2

--- Updater.cs
+++ Updater.cs
@@ -18,7 +18,7 @@ namespace ALL_LEGIT
         private static string RawGitHubUrl = "https://raw.githubusercontent.com/harryeffinpotter/ALL-LEGIT";
         private static string GitHubUrl;
 
-        static readonly public string LocalVersion = "1.1.1";
+        static readonly public string LocalVersion = "1.1.2";
         public static string currentVersion = "";
         public static string currentHFVersion = "";
         public static string changelog = "";
